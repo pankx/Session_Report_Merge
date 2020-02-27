a1da9e6b42574f2973e7e7c5e6a2cc2780603165
pankaj <pankaj@anandpk>
2020-02-27 16:40:55 +0530

    Second_commit

--- Testing_Git_Project/src/Git_Repo_Commit/Frist_Git_Project.java
+++ Testing_Git_Project/src/Git_Repo_Commit/Frist_Git_Project.java
@@ -3,7 +3,9 @@ package Git_Repo_Commit;
 public class Frist_Git_Project {
 
 	public static void main(String[] args) {
-		System.out.println("My frist Git project");
+		
+		System.out.println("My frist Git Commit");
+		System.out.println("My Second Git Commit");
 
 	}
 
