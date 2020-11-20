d1f87bfed1e9c7c18ceb347602f9cc9f82c3aa2b
pankaj <pankaj@anandpk>
2020-11-20 19:03:55 +0530

    this is my new version

--- Testing_Git_Project/src/Git_Repo_Commit/Frist_Git_Project.java
+++ Testing_Git_Project/src/Git_Repo_Commit/Frist_Git_Project.java
@@ -1,11 +1,48 @@
 package Git_Repo_Commit;
 
+import java.io.File;
+import java.util.Arrays;
+import java.util.Date;
+
+import org.apache.commons.io.comparator.LastModifiedFileComparator;
+
 public class Frist_Git_Project {
 
 	public static void main(String[] args) {
-		System.out.println("My frist Git Commit");
-		System.out.println("My Second Git Commit");
+		File dir = new File("/home/pankaj/workSpace/BOAutomation/Download Reports/CSV");
+		
+		File[] files = dir.listFiles();
 
+		System.out.println("Sort files in ascending order base on last modification date");
+
+		Arrays.sort(files, LastModifiedFileComparator.LASTMODIFIED_COMPARATOR);
+		for (int i = 0; i < files.length; i++) {
+			File file = files[i];
+			System.out.printf("File: %s - " + new Date(file.lastModified()) + "\n", file.getName());
+		}
+
+		System.out.println("\nSort files in descending order base on last modification date");
+
+		Arrays.sort(files, LastModifiedFileComparator.LASTMODIFIED_REVERSE);
+		System.out.println(files[0].getName().split("[.]")[1]+"  formate");
+		
+		try {
+		    boolean deleted = dir.delete();
+		    if (files[0].delete()) {
+		        System.out.println("Directory removed.");
+		    } else {
+		        System.out.println("Directory could not be removed");
+		    }
+		} catch (SecurityException ex) {
+		    System.out.println("Delete is denied.");
+		}
+
+		
+	
+		for (int i = 0; i < files.length; i++) {
+			File file = files[i];
+			System.out.printf("File: %s - " + new Date(file.lastModified()) + "\n", file.getName());
+		}
 	}
 
 }
